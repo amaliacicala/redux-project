4d045c12d2c629fb2f3b88cc20331f8b4288acc7
Amalia Cicala <cicala.amalia@gmail.com>
2022-09-15 20:12:01 +0200

    feat: create store object

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
 let lastId = 0;
 
-function reducer(state = [], action) {
+export default function reducer(state = [], action) {
 	switch (action.type) {
 		case 'bugAdded': // case for adding a bug
 			return [
